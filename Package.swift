633a29fd06f77b4cfb6fc200a2861411d8f7ea13
Alex Basson <alexbasson@gmail.com>
2017-02-10 22:13:19 -0500

    Import Quick/Nimble via SwiftPM instead of Cocoapods

--- Package.swift
+++ Package.swift
@@ -1,5 +1,9 @@
 import PackageDescription
 
 let package = Package(
-    name: "SwiftDIHLP"
+    name: "SwiftDIHLP",
+    dependencies: [
+        .Package(url: "https://github.com/Quick/Quick.git", majorVersion: 1),
+        .Package(url: "https://github.com/Quick/Nimble.git", majorVersion: 6)
+    ]
 )
--- Pods/Target Support Files/Pods-SwiftDIHLPTests/Pods-SwiftDIHLPTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_SwiftDIHLPTests : NSObject
-@end
-@implementation PodsDummy_Pods_SwiftDIHLPTests
-@end
